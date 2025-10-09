c10f0bdb022ddfdf8f12c54299def3b58564d215
youssefadly <youssefadly332@gmail.com>
2025-10-09 16:26:10 +0300

    add AssetsData

---
+++ lib/core/utils/asset_data.dart
@@ -0,0 +1,16 @@
+abstract class AssetData {
+  static const String imagesAvatar1 = 'assets/images/avatar1.jpg';
+  static const String imagesAvatar2 = 'assets/images/avatar2.jpg';
+  static const String imagesBalance = 'assets/images/balance.jpg';
+  static const String imagesDashboard = 'assets/images/dashboard.png';
+  static const String imagesExpneses = 'assets/images/expneses.png';
+  static const String imagesGallery = 'assets/images/gallery.png';
+  static const String imagesIncome = 'assets/images/income.png';
+  static const String imagesInvestments = 'assets/images/investments.png';
+  static const String imagesLogout = 'assets/images/logout.png';
+  static const String imagesMaskGroup = 'assets/images/Mask group.png';
+  static const String imagesSetting2 = 'assets/images/setting-2.png';
+  static const String imagesStatistics = 'assets/images/statistics.png';
+  static const String imagesTransaction = 'assets/images/transaction.png';
+  static const String imagesWalletAccount = 'assets/images/wallet_account.png';
+}
