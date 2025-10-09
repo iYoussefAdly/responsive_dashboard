86ab9771007ed20fa9941e2022704f2cc34648b3
youssefadly <youssefadly332@gmail.com>
2025-10-09 15:41:23 +0300

    add AppStyle

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/rendering.dart';
+
+abstract class AppStyles {
+  static const styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    color: Color(0xff064061),
+  );
+  static const styleMediium16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    color: Color(0xff064061),
+  );
+  static const styleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064061),
+  );
+  static const styleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064061),
+  );
+  static const styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    color: Color(0xffA7A7A7),
+  );
+  static const styleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4CB2EB),
+  );
+  static const styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    color: Color(0xffA7A7A7),
+  );
+  static const styleSemiBold18 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0xffF8F8F8),
+  );
+  static const styleBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+    color: Color(0xff4CB3ED),
+  );
+  static const styleMediium20 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    color: Color(0xffF8F8F8),
+  );
+}
---
+++ lib/views/widgets/adaptive_layout.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout({
+    super.key,
+    required this.mobileLayout,
+    required this.tabletLayout,
+    required this.deskTopLayout,
+  });
+  final WidgetBuilder mobileLayout, tabletLayout, deskTopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        if (constrains.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constrains.maxWidth >= 600 && constrains.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return deskTopLayout(context);
+        }
+      },
+    );
+  }
+}
